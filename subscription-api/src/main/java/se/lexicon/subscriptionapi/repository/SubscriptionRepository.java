5c6d888203ee5856240126e39081ed5bf246678d
Halamohamed <halaalimohammed@gmail.com>
2026-03-16 11:52:49 +0100

    added SubscriptionRepository

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/SubscriptionRepository.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/SubscriptionRepository.java
@@ -3,7 +3,6 @@ package se.lexicon.subscriptionapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.subscriptionapi.domain.constant.ServiceType;
 import se.lexicon.subscriptionapi.domain.entity.Customer;
-import se.lexicon.subscriptionapi.domain.entity.Plan;
 import se.lexicon.subscriptionapi.domain.entity.Subscription;
 
 import java.util.List;
