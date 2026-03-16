3a0f580e77a2317dbb9343d9ac48e4b6d3f66d06
Halamohamed <halaalimohammed@gmail.com>
2026-03-16 11:12:30 +0100

    Initial commit

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Plan.java
@@ -0,0 +1,37 @@
+package se.lexicon.subscriptionapi.domain.entity;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "plans")
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotNull
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private ServiceType serviceType;
+    private Integer dataLimit;
+    private boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "operator_id")
+    private Operator operator;
+
+
+}
