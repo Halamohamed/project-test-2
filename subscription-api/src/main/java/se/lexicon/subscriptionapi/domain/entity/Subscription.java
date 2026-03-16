92fab1c5f97dacf3327880396b88b74cd87c9a13
Halamohamed <halaalimohammed@gmail.com>
2026-03-16 11:29:51 +0100

    added Subscription

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/domain/entity/Subscription.java
@@ -0,0 +1,38 @@
+package se.lexicon.subscriptionapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import se.lexicon.subscriptionapi.domain.constant.SubscriptionStatus;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @ManyToOne
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus status;
+
+    private LocalDateTime cancellationDate;
+
+
+
+
+}
