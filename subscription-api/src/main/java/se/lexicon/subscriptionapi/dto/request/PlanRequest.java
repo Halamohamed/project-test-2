82ca67ee2f1e6e26ec13b75b6e1354cf42e7f3d2
Halamohamed <halaalimohammed@gmail.com>
2026-03-16 12:15:17 +0100

    added PlanDTO

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/dto/request/PlanRequest.java
@@ -0,0 +1,17 @@
+package se.lexicon.subscriptionapi.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+
+import java.math.BigDecimal;
+
+public record PlanRequest(
+        @NotBlank String name,
+        @NotNull BigDecimal price,
+        @NotBlank ServiceType serviceType,
+        Integer dataLimit,
+        boolean isActive,
+        @NotNull Long operatorId
+) {
+}
---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/dto/response/PlanResponse.java
@@ -0,0 +1,16 @@
+package se.lexicon.subscriptionapi.dto.response;
+
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+
+import java.math.BigDecimal;
+
+public record PlanResponse(
+        Long id,
+        String name,
+        BigDecimal price,
+        ServiceType serviceType,
+        Integer dataLimit,
+        boolean isActive,
+        String operatorName
+) {
+}
