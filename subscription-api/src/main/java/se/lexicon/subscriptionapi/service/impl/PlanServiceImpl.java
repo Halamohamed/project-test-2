88f8dd82cb5dcde6b6a356b043dd7d8a9e37698a
Halamohamed <halaalimohammed@gmail.com>
2026-03-17 11:47:07 +0100

    added SubscriptionServiceImpl

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/service/impl/PlanServiceImpl.java
@@ -0,0 +1,95 @@
+package se.lexicon.subscriptionapi.service.impl;
+
+import org.springframework.stereotype.Service;
+import se.lexicon.subscriptionapi.domain.entity.Operator;
+import se.lexicon.subscriptionapi.domain.entity.Plan;
+import se.lexicon.subscriptionapi.dto.request.PlanRequest;
+import se.lexicon.subscriptionapi.dto.response.PlanResponse;
+import se.lexicon.subscriptionapi.exception.BusinessRuleException;
+import se.lexicon.subscriptionapi.exception.ResourceNotFoundException;
+import se.lexicon.subscriptionapi.mapper.PlanMapper;
+import se.lexicon.subscriptionapi.repository.OperatorRepository;
+import se.lexicon.subscriptionapi.repository.PlanRepository;
+import se.lexicon.subscriptionapi.service.PlanService;
+
+import java.util.List;
+@Service
+public class PlanServiceImpl implements PlanService {
+
+    private final PlanMapper planMapper;
+    private final OperatorRepository operatorRepository;
+    private final PlanRepository planRepository;
+
+    public PlanServiceImpl(PlanMapper planMapper,
+                           OperatorRepository operatorRepository,
+                           PlanRepository planRepository) {
+        this.planMapper = planMapper;
+        this.operatorRepository = operatorRepository;
+        this.planRepository = planRepository;
+    }
+
+    @Override
+    public PlanResponse createPlan(PlanRequest planRequest) {
+        Operator operator = operatorRepository.findById(planRequest.operatorId())
+                .orElseThrow(()-> new ResourceNotFoundException("Operator not found"));
+        boolean exists = planRepository.existsByNameAndOperatorId(
+                planRequest.name(), planRequest.operatorId());
+        if (exists){
+            throw new BusinessRuleException("Plan name already exists for this operator");
+        }
+        Plan plan = planMapper.toEntity(planRequest);
+        plan.setOperator(operator);
+        Plan savedPlan =planRepository.save(plan);
+
+        return planMapper.toResponse(savedPlan);
+    }
+
+    @Override
+    public PlanResponse updatePlan(Long id, PlanRequest planRequest) {
+       Plan plan = planRepository.findById(id)
+               .orElseThrow(()-> new ResourceNotFoundException("Plan not found"));
+       Operator operator = operatorRepository.findById(planRequest.operatorId())
+               .orElseThrow(()-> new ResourceNotFoundException("Operator not found"));
+
+       boolean exists = planRepository.existsByNameAndOperatorIdAndIdNot(
+               planRequest.name(), planRequest.operatorId(), id);
+
+       if (exists){
+           throw new BusinessRuleException("Another plan with this name already exists for this operator");
+       }
+       plan.setName(planRequest.name());
+       plan.setPrice(planRequest.price());
+       plan.setServiceType(planRequest.serviceType());
+       plan.setDataLimit(planRequest.dataLimit());
+       plan.setActive(planRequest.isActive());
+       plan.setOperator(operator);
+
+        return planMapper.toResponse(plan);
+
+    }
+
+    @Override
+    public void deletePlan(Long id) {
+        if(!planRepository.existsById(id)){
+            throw new ResourceNotFoundException("Plan not found");
+        }
+        planRepository.deleteById(id);
+
+    }
+
+    @Override
+    public List<PlanResponse> findAll() {
+        return planRepository.findAll()
+                .stream()
+                .map(planMapper::toResponse)
+                .toList();
+    }
+
+    @Override
+    public List<PlanResponse> findActivePlan() {
+        return planRepository.findByIsActiveTrue()
+                .stream()
+                .map(planMapper::toResponse)
+                .toList();
+    }
+}
