3dcd9c310cde63af20a21c9a17e7b15c5cee2fcf
Halamohamed <halaalimohammed@gmail.com>
2026-03-17 11:44:04 +0100

    updated repo

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/PlanRepository.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/PlanRepository.java
@@ -1,5 +1,7 @@
 package se.lexicon.subscriptionapi.repository;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.subscriptionapi.domain.entity.Plan;
 
@@ -9,4 +11,8 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     List<Plan> findByIsActiveTrue();
     List<Plan> findByOperatorIdAndIsActiveTrue(Long operatorId);
+
+    boolean existsByNameAndOperatorId(@NotBlank String name, @NotNull Long operatorId);
+
+    boolean existsByNameAndOperatorIdAndIdNot(@NotBlank String name, @NotNull Long operatorId, Long id);
 }
--- subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/SubscriptionRepository.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/SubscriptionRepository.java
@@ -2,6 +2,7 @@ package se.lexicon.subscriptionapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+import se.lexicon.subscriptionapi.domain.constant.SubscriptionStatus;
 import se.lexicon.subscriptionapi.domain.entity.Customer;
 import se.lexicon.subscriptionapi.domain.entity.Subscription;
 
@@ -11,4 +12,5 @@ import java.util.Optional;
 public interface SubscriptionRepository extends JpaRepository<Subscription,Long> {
     Optional<Subscription> findByCustomerAndPlan_ServiceType(Customer customer, ServiceType serviceType);
     List<Subscription> findByCustomer(Customer customer);
+    boolean existsByCustomerIdAndPlan_ServiceTypeAndStatus(Long customerId, ServiceType serviceType, SubscriptionStatus status);
 }
