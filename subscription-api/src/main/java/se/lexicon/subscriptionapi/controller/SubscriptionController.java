40a007ea05bd4e3d689f240e52d5a3203fb4d180
Halamohamed <halaalimohammed@gmail.com>
2026-03-18 11:46:27 +0100

    added SubscriptionController

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/controller/SubscriptionController.java
@@ -0,0 +1,46 @@
+package se.lexicon.subscriptionapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.subscriptionapi.dto.request.SubscriptionRequest;
+import se.lexicon.subscriptionapi.dto.response.SubscriptionResponse;
+import se.lexicon.subscriptionapi.service.SubscriptionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/subscripions")
+@RequiredArgsConstructor
+public class SubscriptionController {
+
+    private final SubscriptionService subscriptionService;
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PostMapping
+    public ResponseEntity<SubscriptionResponse> subscribe(@RequestParam Long customerId,
+                                                          @RequestBody SubscriptionRequest subscriptionRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionService.subscribe(customerId, subscriptionRequest));
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    public ResponseEntity<List<SubscriptionResponse>> findByCustomerId(@PathVariable Long customerId) {
+       return ResponseEntity.ok(subscriptionService.findByCustomer(customerId));
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/{subscriptionId}/change/{newPlanId}")
+    public ResponseEntity<SubscriptionResponse> changePlan(@PathVariable Long subscriptionId,
+                                                           @PathVariable Long newPlanId) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionService.changePlan(subscriptionId, newPlanId));
+    }
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/{subscriptionId}/cancel")
+    public ResponseEntity<Void> cancel(@PathVariable Long subscriptionId) {
+        subscriptionService.cancel(subscriptionId);
+        return ResponseEntity.noContent().build();
+    }
+}
