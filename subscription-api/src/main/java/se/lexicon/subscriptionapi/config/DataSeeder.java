6a61678e7ed4c0c5568269fcde0f225d5ad76caf
Halamohamed <halaalimohammed@gmail.com>
2026-03-19 13:53:53 +0100

    added seedOperator

--- subscription-api/src/main/java/se/lexicon/subscriptionapi/config/DataSeeder.java
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/config/DataSeeder.java
@@ -5,9 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import se.lexicon.subscriptionapi.domain.constant.Role;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
 import se.lexicon.subscriptionapi.domain.entity.Customer;
+import se.lexicon.subscriptionapi.domain.entity.Operator;
+import se.lexicon.subscriptionapi.domain.entity.Plan;
 import se.lexicon.subscriptionapi.repository.CustomerRepository;
+import se.lexicon.subscriptionapi.repository.OperatorRepository;
+import se.lexicon.subscriptionapi.repository.PlanRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -16,11 +24,14 @@ public class DataSeeder implements CommandLineRunner {
 
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
+    private final PlanRepository planRepository;
+    private final OperatorRepository operatorRepository;
 
     @Override
     public void run(String... args) throws Exception {
         seedAdminUser();
         seedRegularUser();
+        seedOperator();
     }
 
     private void seedAdminUser() {
@@ -50,5 +61,34 @@ public class DataSeeder implements CommandLineRunner {
             System.out.println("[DATA_SEED] Regular user created: " + userEmail);
         }
     }
+
+    private void seedOperator(){
+        if (operatorRepository.count() > 0) {
+            return;
+        }
+        Operator operator1 = new Operator();
+        operator1.setName("Fiber");
+        operator1.setCreatedAt(LocalDateTime.now());
+        operator1.setPlans(List.of(new Plan(1L,"Fiber 50", 200.99, ServiceType.INTERNET,50, true,operator1),
+                new Plan(2L, "Fiber 100", 390.99,  ServiceType.INTERNET,100, true,operator1),
+                new Plan(3L, "Fiber 300", 590.99, ServiceType.INTERNET, 300, false, operator1)));
+        Operator operator2 = new Operator();
+        operator2.setName("Mobile");
+        operator2.setCreatedAt(LocalDateTime.now());
+        operator2.setPlans(List.of(new Plan(4L, "Mobile Basic", 190.99,  ServiceType.MOBILE, 5, true, operator2),
+                new Plan(5L, "Mobile Plus", 290.99,   ServiceType.MOBILE, 20, true, operator2),
+                new Plan(6L, "Mobile Unlimited", 490.99, ServiceType.MOBILE, null, false, operator2)));
+
+        operatorRepository.save(operator1);
+        operatorRepository.save(operator2);
+
+        // planRepository.save(new Plan(5L,"Fiber 50", 200.99, ServiceType.INTERNET,50, true,operator1));
+       /* planRepository.save(new Plan(2L, "Fiber 100", 390.99,  ServiceType.INTERNET,100, true,operator1));
+        planRepository.save(new Plan(3L, "Fiber 300", 590.99, ServiceType.INTERNET, 300, false, operator1));
+*/
+       // planRepository.save(new Plan(4L, "Mobile Basic", 190.99,  ServiceType.MOBILE, 5, true, operator2));
+      /*  planRepository.save(new Plan(5L, "Mobile Plus", 290.99,   ServiceType.MOBILE, 20, true, operator2));
+        planRepository.save(new Plan(6L, "Mobile Unlimited", 490.99, ServiceType.MOBILE, null, false, operator2));
+   */ }
 
 }
